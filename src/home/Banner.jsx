0cf509d7157210aecc6d6394ddba0a1e59354b70
Endriyani Rahayu <endriyanirahayu@192.168.40.57>
2024-07-02 18:04:34 +0700

    added selected category for home banner

--- src/home/Banner.jsx
+++ src/home/Banner.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import productData from "../products.json";
 import { Link } from "react-router-dom";
+import SelectedCategory from "../components/SelectedCategory";
 
 const title = (
   <h2>
@@ -33,6 +34,7 @@ const Banner = () => {
         <div className="banner-content">
           {title}
           <form>
+            <SelectedCategory select={"all"} />
             <input
               type="text"
               name="search"
