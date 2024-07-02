13c00b06041373799f2337c83ba5dc9643a175f4
Endriyani Rahayu <endriyanirahayu@192.168.40.57>
2024-07-02 18:52:14 +0700

    added home category section card

--- src/components/NavItems.jsx
+++ src/components/NavItems.jsx
@@ -33,64 +33,66 @@ const NavItems = () => {
         </div>
       </div>
       {/* Header bottom start */}
-      <div className="container">
-        <div className="header-wrapper">
-          {/* logo */}
-          <div className="logo-search-acts">
-            <div className="logo">
-              <Link to={"/"}>
-                <img src={logo} alt="logo" />
+      <div className="header-bottom">
+        <div className="container">
+          <div className="header-wrapper">
+            {/* logo */}
+            <div className="logo-search-acts">
+              <div className="logo">
+                <Link to={"/"}>
+                  <img src={logo} alt="logo" />
+                </Link>
+              </div>
+            </div>
+
+            {/* menu area */}
+            <div className="menu-area">
+              <div className="menu">
+                <ul className={`lab-ul ${menuToggle && "active"}`}>
+                  <li>
+                    <Link to="/">Home</Link>
+                  </li>
+                  <li>
+                    <Link to="/shop">Shop</Link>
+                  </li>
+                  <li>
+                    <Link to="/blog">Blog</Link>
+                  </li>
+                  <li>
+                    <Link to="/about">About</Link>
+                  </li>
+                  <li>
+                    <Link to="/contact">Contact</Link>
+                  </li>
+                </ul>
+              </div>
+
+              {/* Sign up and LogIn */}
+              <Link to={"/sign-up"} className="lab-btn me-3 d-none d-md-block">
+                Create Account
               </Link>
-            </div>
-          </div>
+              <Link to={"login"} className="d-none d-md-block">
+                Log In
+              </Link>
 
-          {/* menu area */}
-          <div className="menu-area">
-            <div className="menu">
-              <ul className={`lab-ul ${menuToggle && "active"}`}>
-                <li>
-                  <Link to="/">Home</Link>
-                </li>
-                <li>
-                  <Link to="/shop">Shop</Link>
-                </li>
-                <li>
-                  <Link to="/blog">Blog</Link>
-                </li>
-                <li>
-                  <Link to="/about">About</Link>
-                </li>
-                <li>
-                  <Link to="/contact">Contact</Link>
-                </li>
-              </ul>
-            </div>
+              {/* Menu Toggler */}
+              <div
+                onClick={() => setMenuToggle(!menuToggle)}
+                className={`header-bar d-lg-none ${menuToggle && "active"}`}
+              >
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+              </div>
 
-            {/* Sign up and LogIn */}
-            <Link to={"/sign-up"} className="lab-btn me-3 d-none d-md-block">
-              Create Account
-            </Link>
-            <Link to={"login"} className="d-none d-md-block">
-              Log In
-            </Link>
-
-            {/* Menu Toggler */}
-            <div
-              onClick={() => setMenuToggle(!menuToggle)}
-              className={`header-bar d-lg-none ${menuToggle && "active"}`}
-            >
-              <span></span>
-              <span></span>
-              <span></span>
-              <span></span>
-            </div>
-
-            {/* Social Toggler */}
-            <div
-              className="ellepsis-bar d-md-none"
-              onClick={() => setSocialToggle(!socialToggle)}
-            >
-              <i className="icofont-info-square"></i>
+              {/* Social Toggler */}
+              <div
+                className="ellepsis-bar d-md-none"
+                onClick={() => setSocialToggle(!socialToggle)}
+              >
+                <i className="icofont-info-square"></i>
+              </div>
             </div>
           </div>
         </div>
---
+++ src/home/HomeCategory.jsx
@@ -0,0 +1,87 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const title = "Buy Everything with Us";
+const subTitle = "Choose Any Products";
+const btnText = "Get Started Now";
+const categoryList = [
+  {
+    imgUrl: "src/assets/images/category/01.jpg",
+    imgAlt: "category endriyani10 endriyani",
+    iconName: "icofont-brand-windows",
+    title: "DSLR Camera",
+  },
+  {
+    imgUrl: "src/assets/images/category/02.jpg",
+    imgAlt: "category endriyani10 endriyani",
+    iconName: "icofont-brand-windows",
+    title: "Shoes",
+  },
+  {
+    imgUrl: "src/assets/images/category/03.jpg",
+    imgAlt: "category endriyani10 endriyani",
+    iconName: "icofont-brand-windows",
+    title: "Photography",
+  },
+  {
+    imgUrl: "src/assets/images/category/04.jpg",
+    imgAlt: "category endriyani10 endriyani",
+    iconName: "icofont-brand-windows",
+    title: "Formal Dress",
+  },
+  {
+    imgUrl: "src/assets/images/category/05.jpg",
+    imgAlt: "category endriyani10 endriyani",
+    iconName: "icofont-brand-windows",
+    title: "Colorful Bags",
+  },
+  {
+    imgUrl: "src/assets/images/category/06.jpg",
+    imgAlt: "category endriyani10 endriyani",
+    iconName: "icofont-brand-windows",
+    title: "Home Decor",
+  },
+];
+const HomeCategory = () => {
+  return (
+    <div className="category-section style-4 padding-tb">
+      <div className="container">
+        {/* section header */}
+        <div className="section-header text-center">
+          <span className="subtitle">{subTitle}</span>
+          <h2 className="title">{title}</h2>
+        </div>
+
+        {/* section card */}
+        <div className="section-wrapper">
+          <div className="row g-5 justify-content-center row-cols-md-3 row-cols-sm-2 row-cols-1">
+            {categoryList.map((category, i) => (
+              <div key={`category-${i}`}>
+                <Link to="/shop" className="category-item">
+                  <div className="category-inner">
+                    {/* image thumbail */}
+                    <div className="category-thumb">
+                      <img src={category.imgUrl} alt={category.imgAlt} />
+                    </div>
+
+                    {/* content */}
+                    <div className="category-content">
+                      <div className="cate-icon">
+                        <i className={category.iconName}></i>
+                      </div>
+                      <Link to="/shop">
+                        <h6>{category.title}</h6>
+                      </Link>
+                    </div>
+                  </div>
+                </Link>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HomeCategory;
